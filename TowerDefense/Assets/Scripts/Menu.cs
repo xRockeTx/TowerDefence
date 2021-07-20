e5e6c7b3a40e6788393aaa77a257e69bbb9b4e4d
xRockeTx <66239537+xRockeTx@users.noreply.github.com>
2021-07-20 19:11:15 +0300

    Меню 1

--- TowerDefense/Assets/Scripts/Menu.cs
+++ TowerDefense/Assets/Scripts/Menu.cs
@@ -7,12 +7,17 @@ using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-    [SerializeField] private GameObject PanelWithLevels;
+    [SerializeField] private GameObject PanelWithLevels,PanelWithSettings;
 
     public void OpenOrCloseLevelPan()
     {
         PanelWithLevels.SetActive(!PanelWithLevels.activeSelf);
     }
+    public void OpenOrCloseSetting()
+    {
+        PanelWithSettings.SetActive(!PanelWithSettings.activeSelf);
+    }
+
     public void GoToLevel(int level)
     {
         SceneManager.LoadScene($"Lvl{level}");
